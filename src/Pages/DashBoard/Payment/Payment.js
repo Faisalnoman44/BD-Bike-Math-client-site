4c7657faa32a24d6d407a8b61c337bb9745a2044
Faisalnoman44 <mdnoman.mn11@gmail.com>
2022-11-30 21:40:51 +0600

    stripe install and checkout page create

--- src/Pages/DashBoard/Payment/Payment.js
+++ src/Pages/DashBoard/Payment/Payment.js
@@ -1,9 +1,25 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import CheckOutForm from './CheckOutForm';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+console.log(stripePromise);
 
 const Payment = () => {
+    const bookings = useLoaderData()
+    console.log(bookings);
+    const { bikeName, bikePrice } = bookings
+
     return (
         <div>
-            <h1>Payment</h1>
+            <h1 className='text-2xl my-4'>Payment For {bikeName}</h1>
+            <div className='w-full my-10 border border-primary p-5'>
+                <Elements stripe={stripePromise}>
+                    <CheckOutForm />
+                </Elements>
+            </div>
         </div>
     );
 };
--- src/Pages/ErrorPage/ErrorPage.js
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -1,16 +1,26 @@
+import { useContext } from "react";
 import { useRouteError } from "react-router-dom";
+import { AuthContext } from "../../Context/AuthProvider";
 
 export default function ErrorPage() {
     const error = useRouteError();
     console.error(error);
+    const {logOut} = useContext(AuthContext)
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(err => console.log(err))
+    }
 
     return (
         <div id="error-page" className="text-center text-lg md:text-2xl">
-                <h1>Oops!</h1>
-                <p>Sorry, an unexpected error has occurred.</p>
-                <p>
-                    <i>{error.statusText || error.message}</i>
-                </p>
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+                <i>{error.statusText || error.message}</i>
+            </p>
+            <p>please <button className="text-primary" onClick={handleLogOut}>Logout</button></p>
         </div>
     );
 }
